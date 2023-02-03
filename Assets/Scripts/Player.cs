af6ecff42499ad64f9223af8d1140e4d4c209a96
SergeyKiselev <sergey.kiselev98@mail.ru>
2023-02-03 22:54:45 +0500

    mover

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private Transform rotateObject;
     private Rigidbody rigidbody;
     private Vector3 vector3;
+    private Vector3 vector3Rotate;
+    public float speedRotate = 10f;
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -13,12 +16,17 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.AddForce(vector3 * _speed);
+        rotateObject.rotation = Quaternion.Lerp(rotateObject.rotation,Quaternion.LookRotation(vector3Rotate),Time.deltaTime * speedRotate);
     }
     public void SetDirection(Vector3 vector)
     {
-      // rigidbody.velocity = vector * _speed;
-      vector3 = vector;
-        Debug.Log(vector);
+        vector3 = vector;
+        if(vector != Vector3.zero)
+        {
+            vector3Rotate = vector;
+        }
     }
+
+
 
 }
