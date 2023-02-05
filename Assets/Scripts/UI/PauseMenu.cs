679639dc5fdfb95d024f9cb1fedd9a113c22fef7
SergeyKiselev <sergey.kiselev98@mail.ru>
2023-02-05 12:54:58 +0500

    gameover

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -10,21 +10,32 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject m_PauseMenu;
     [SerializeField] private Image m_Image;
     [SerializeField] private bool isPause = false;
+    [SerializeField] private GameObject m_GameOverMenu;
 
     private void Start()
     {
         Time.timeScale = 1;
     }
+    public void GameOver(Slider value)
+    {
+        Debug.Log(value.value);
+        if(value.value != 0) return;
+        isPause = true;
+        if (!isPause) Time.timeScale = 1;
+        m_GameOverMenu.SetActive(true);
+        StartCoroutine(SetAlphaMax(isPause));
+    }
     public void ToggleMenu()
     {
         isPause = !isPause;
         if(!isPause) Time.timeScale = 1;
+        m_PauseMenu.SetActive(isPause);
         StartCoroutine(SetAlphaMax(isPause));
 
     }
     IEnumerator SetAlphaMax(bool toMax)
     {
-        m_PauseMenu.SetActive(isPause);
+//        m_PauseMenu.SetActive(isPause);
         float curTime = 0;
         float time = 0.5f;
         float shag = 0.05f;
@@ -42,5 +53,6 @@ public class PauseMenu : MonoBehaviour
         }
         if (isPause) Time.timeScale = 0;
     }
+
 
 }
